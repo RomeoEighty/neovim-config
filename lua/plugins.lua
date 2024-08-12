382706516c0d156b0ad54a4c28ebfc03076f5168
RomeoEighty <major.yamaguchi@gmail.com>
2024-08-13 01:07:02 +0900

    add render-markdown.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -117,6 +117,14 @@ local plugins = {
     },
 
     {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {},
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    },
+
+    {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         ---@module "ibl"
