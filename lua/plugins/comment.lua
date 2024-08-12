cbbddb7b8a5ec0827edba73fda631c2f791c9c1b
RomeoEighty <major.yamaguchi@gmail.com>
2024-08-12 20:43:47 +0900

    returns empty table when plugin is not available

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,8 +1,9 @@
 local status_ok, comment = pcall(require, "Comment")
 if not status_ok then
-    return
+    return {}
 end
 
+-- https://www.reddit.com/r/neovim/comments/18nx8sj/integrate_nvimtscontextcommentstring/
 comment.setup {
     pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 }
