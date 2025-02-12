1df5a4339f64eb876e9ca37b7d50f89499072944
RomeoEighty <major.yamaguchi@gmail.com>
2025-02-12 22:57:37 +0900

    add neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -92,6 +92,20 @@ local plugins = {
         end,
     },
 
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            -- "ibhagwan/fzf-lua",              -- optional
+            -- "echasnovski/mini.pick",         -- optional
+        },
+        config = true
+    },
+
     -- LSP
     {
         'neovim/nvim-lspconfig',
