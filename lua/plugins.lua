d5d9638efea89b56427e4fdf0eb9f9a1c88bd74e
RomeoEighty <major.yamaguchi@gmail.com>
2025-03-06 15:15:37 +0900

    testing copilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -163,6 +163,19 @@ local plugins = {
     },
 
     {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = {
+            -- See Configuration section for options
+        },
+        -- See Commands section for default commands if you want to lazy load on them
+    },
+
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         config = function()
