e296e5ad4517b952f4c777e33ecac2440aa79cd5
RomeoEighty <major.yamaguchi@gmail.com>
2025-03-09 06:01:37 +0900

    cmp shows copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -116,6 +116,7 @@ cmp.setup {
         { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
+        { name = "copilot" },
     },
     confirm_opts = {
         behavior = cmp.ConfirmBehavior.Replace,
