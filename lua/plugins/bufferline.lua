12eaad59743a5e9fd68a82fd9f2a7fc5fd2309ca
RomeoEighty <major.yamaguchi@gmail.com>
2025-05-18 01:48:43 +0900

    change buffeline separator to simple vertical lines

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -94,7 +94,7 @@ bufferline.setup {
         move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
         -- can also be a table containing 2 custom separators
         -- [focused and unfocused]. eg: { '|', '|' }
-        separator_style = "slant", -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
+        separator_style = {"|","|"}, -- "slant" | "slope" | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = false, -- false | true,
         always_show_bufferline = true, -- true | false,
         hover = {
